17307b8ed535e80fc6480d6718a843179b5c321c
vk-24-2006 <vivek.krishnan2006@gmail.com>
2024-10-15 09:31:48 +0530

    Add files via upload

---
+++ Leap year.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int y;
+	printf("Enter the year");
+	scanf("%d",&y);
+	if(y%4==0 || y%400==0)
+	{
+		printf("%d year is a leap year",y);
+	}
+	else if(y%4==0 && y%100==0)
+	{
+		printf("%d year is not a leap year",y);
+	}
+	else
+	{
+		printf("%d is not a leap year",y);
+	}
+	return 0;
+}
