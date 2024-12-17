57611d74b97b2e99a602b918cb248d85f04661ee
vk-24-2006 <vivek.krishnan2006@gmail.com>
2024-12-17 22:53:13 +0530

    Add files via upload

---
+++ even numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n, a;
+	printf("Enter a number :");
+	scanf("%d",&n);
+	for(a=0;a<=n;a++)
+	{
+	if(a%2==0)
+	 {  
+		printf("%d\n",a);
+     }
+    }
+	return 0;
+}
+
+
